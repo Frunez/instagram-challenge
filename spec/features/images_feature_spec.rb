2e599148c97cc61de91be32cf59a83600c758b45
Frunez <francesco.nunez@gmail.com>
2016-09-26 16:48:56 +0100

    added index view for images

--- spec/features/images_feature_spec.rb
+++ spec/features/images_feature_spec.rb
@@ -5,7 +5,20 @@ feature 'images' do
     scenario 'should display a prompt to add upload an image' do
       visit 'images'
       expect(page).to have_content 'No images have been uploaded yet :('
-      expect(page.to have_link 'Upload an image')
+      expect(page).to have_link 'Upload an image'
+    end
+  end
+
+  context 'images have been added' do
+    before do
+      Image.create(name: 'Cat', category: 'Animals')
+    end
+
+    scenario 'displays images' do
+      visit '/images'
+      expect(page).to have_content 'Cat'
+      expect(page).to have_content 'Category: Animals'
+      expect(page).not_to have_content('No images have been uploaded yet :(')
     end
   end
 end
