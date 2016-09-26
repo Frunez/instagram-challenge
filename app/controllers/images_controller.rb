22619f4e698d64dad025a1d1b7f135e8e7401d4d
Frunez <francesco.nunez@gmail.com>
2016-09-26 22:01:45 +0100

    images CRUD completed, associations :construction:

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,4 +2,39 @@ class ImagesController < ApplicationController
   def index
     @images = Image.all
   end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.create(image_params)
+    redirect_to "/images/#{@image.id}"
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    @image.update(image_params)
+    flash[:notice] = 'Image successfully updated'
+    redirect_to "/images/#{@image.id}"
+  end
+
+  def destroy
+    @image = Image.find(params[:id])
+    @image.destroy
+    flash[:notice] = 'Image successfully removed'
+    redirect_to "/images"
+  private
+
+  def image_params
+    params.require(:image).permit(:name, :category)
+  end
 end
--- spec/features/images_feature_spec.rb
+++ spec/features/images_feature_spec.rb
@@ -21,4 +21,54 @@ feature 'images' do
       expect(page).not_to have_content('No images have been uploaded yet :(')
     end
   end
+
+  context 'creating images' do
+    scenario 'prompts user to fill out a form, then displays the new image' do
+      visit '/images'
+      click_link 'Upload an image'
+      fill_in 'Name', with: 'Cat'
+      click_button 'Upload'
+      expect(page).to have_content 'Cat'
+      expect(current_path).to include '/images/'
+    end
+  end
+
+  context 'viewing images' do
+
+    let!(:cat){ Image.create(name: 'Cat', category: 'Animals') }
+
+    scenario 'lets users view a single image' do
+      visit '/images'
+      click_link 'Cat'
+      expect(page).to have_content 'Cat'
+      expect(page).to have_content 'Category: Animals'
+      expect(current_path).to eq "/images/#{cat.id}"
+    end
+  end
+
+  context 'updating images' do
+
+    let!(:car){ Image.create(name: 'Car', category: 'Sports') }
+
+    scenario 'lets a user edit an image' do
+      visit '/images'
+      click_link 'Edit'
+      fill_in 'Name', with: 'Lexus'
+      fill_in 'Category', with: 'Automobiles'
+      click_button 'Update'
+      expect(page).to have_content 'Lexus'
+      expect(page).to have_content 'Category: Automobiles'
+      expect(current_path).to eq "/images/#{car.id}"
+    end
+  end
+
+  context 'deleting images'
+
+    before { Image.create(name: 'Octopus', category: 'Sea') }
+
+    scenario 'removes image when user clicks remove' do
+      visit '/images'
+      click_link 'Remove'
+      expect(page).not_to have_content 'Image successfully removed'
+    end
 end
